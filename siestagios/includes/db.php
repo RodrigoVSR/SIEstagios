e1e877b38618177419d4f6dfaa4b2a68ddd7b756
RodrigoVSR <rvsrobalo@gmail.com>
2026-03-23 09:48:40 +0000

    Add files via upload

---
+++ siestagios/includes/db.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$db   = "siestagios";
+$user = "root";
+$pass = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8",
+        $user,
+        $pass
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na ligação à base de dados: " . $e->getMessage());
+}
